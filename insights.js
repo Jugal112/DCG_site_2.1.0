5dd2fdae3bd65d1b4c0dd58704184ffe61f9a165
jugal112 <jugal112@gmail.com>
2020-12-14 13:38:44 -0500

    Added range sliders to Analytics Page

--- insights.js
+++ insights.js
@@ -19,8 +19,8 @@ var colorCodedCols = new Set([
     '3mo %Chg', 
     '12mo %Chg', 
     'Avg QoQ Rev Growth', 
-    'QoQ Rev Surprise'
-])
+    'QoQ Rev Surprise',
+]);
 var currencyCols = new Set([
     "Revenue",
     "Market Cap",
@@ -36,7 +36,6 @@ var percentCols = new Set([
     "12mo %Chg", 
     "Avg QoQ Rev Growth",
     "QoQ Rev Surprise",
-    "Gross Margin",
     "LTM Gross Margin",
     "LTM EBITDA Margin",
     "LTM FCF Margin"
@@ -55,115 +54,128 @@ function myFormatter(column, value) {
     // return d3.format(".2s")(value);
 }
 
-d3.csv("/data.csv").then(function(data) {
-    // Set data as global for debugging.
-    console.log(numericCols);
+var filters = {};
 
-    // Select the div with the "table" id.
-    var table = d3.select("#table")
-    table.append("table")
-    // Append a header created from the data's columns.
-    var header = table.append("thead").append("tr");
-    var sortAscending = true;
-    header
-        .selectAll("th")
-        .data(data.columns)
-        .enter()
-        .append("th")
-        .text(function(d) { return d; })
-        .on('click', function (d) {
-            header.attr('class', 'header');
-            column = this.textContent;
-            var order = sortAscending ? 1 : -1;
-            rows.sort(function(a, b) {
-                sortFn = numericCols.has(column) ? mySortNumber : mySort;
-                return order * sortFn(a[column], b[column]);  
+d3.csv("/data.csv").then(function (data) {
+    numericCols.forEach(function (column) {
+        createRangeSlider(data, column);
+    });
+})
+
+function render() {
+    d3.csv("/data.csv").then(function (data) {
+        // Select the div with the "table" id.
+        d3.selectAll("table").remove();
+        var table_container = d3.select("#box").append("table")
+        var table = d3.select("table");
+        // Append a header created from the data's columns.
+        var header = table.append("thead").append("tr");
+        var sortAscending = true;
+        header
+            .selectAll("th")
+            .data(data.columns)
+            .enter()
+            .append("th")
+            .text(function(d) { return d; })
+            .on('click', function (d) {
+                header.attr('class', 'header');
+                column = this.textContent;
+                var order = sortAscending ? 1 : -1;
+                rows.sort(function(a, b) {
+                    sortFn = numericCols.has(column) ? mySortNumber : mySort;
+                    return order * sortFn(a[column], b[column]);  
+                });
+                this.className = sortAscending ? 'aes' : 'des';
+                sortAscending = !sortAscending;
             });
-            this.className = sortAscending ? 'aes' : 'des';
-            sortAscending = !sortAscending;
-        });
-    var tablebody = table.append('tbody');
-    var rows = tablebody
-        .selectAll("tr")
-        .data(data)
-        .enter()
-        .append("tr");
-
-    d3.select("#mySlider").on("change", function(d){
-        selectedValue = this.value;
-        console.log(selectedValue);
-        rows.filter(function(d) {
-            
-        });
-        console.log(rows);
-        })
-
-    // We built the rows using the nested array - now each row has its own array.
-    var cells = rows.selectAll("td")
-        // each row has data associated; we get it and enter it for the cells.
-        .data(function(row) {
-            new_row = data.columns.map(function(col) {
-                return { column: col, value: row[col], display: myFormatter(col, row[col])}
+        var tablebody = table.append('tbody');
+        var rows = tablebody
+            .selectAll("tr")
+            .data(data)
+            .enter()
+            .filter(function (row) {
+                for (col in filters) {
+                    min = filters[col][0];
+                    max = filters[col][1];
+                    value = parseFloat(row[col]);
+                    between = (min <= value && value <= max)
+                    if (!between) {
+                        return false;
+                    }
+                }
+                return true;
             })
-            return new_row;
+            .append("tr");
+        rows.exit().remove();
+
+        var cells = rows.selectAll("td")
+            .data(function(row) {
+                new_row = data.columns.map(function(col) {
+                    return { column: col, value: row[col], display: myFormatter(col, row[col])}
+                })
+                return new_row;
+            })
+            .enter()
+            .append("td")
+            .text(function(d) { return d.display; })
+            .style("background-color", function(d){
+                if (!colorCodedCols.has(d.column)) {
+                    return
+                }
+                let x = parseFloat(d.value);
+                if (x > 0) {
+                    return '#BBFFBB';
+                } else if (x < 0) {
+                    return '#FFBBBB';
+                };
+            });
+        cells.exit().remove();
+    });    
+};
+
+render();
+
+function createRangeSlider(data, column) {
+    var settings_selection = d3.select("#settings");
+    var div = settings_selection.append("div");
+    div.append('p').text(column);
+
+    var revs = data.map( function(d) {
+        return parseFloat(d[column]);});
+    
+    minimum = d3.min(revs);
+    maximum = d3.max(revs);
+    var sliderRange = d3
+        .sliderBottom()
+        .min(minimum)
+        .max(maximum)
+        .width(300)
+        .tickFormat(function(d) {
+            return myFormatter(column, d)
         })
-        .enter()
-        .append("td")
-        .text(function(d) { return d.display; })
-        .style("background-color", function(d){
-            if (!colorCodedCols.has(d.column)) {
-                return
-            }
-            let x = parseFloat(d.value);
-            if (x > 0) {
-                return '#BBFFBB';
-            } else if (x < 0) {
-                return '#FFBBBB';
-            };
+        .ticks(1)
+        .default([minimum,maximum])
+        .fill('#2196f3')
+        .on('onchange', val => {
+            // d3.select('p#value-range')
+            //     .text(val.map(function (d) {return myFormatter(column, d)})
+            //     .join('-'));
+            filters[column] = val;
+            render();
         });
 
-    console.log(cells)
-  });
+    var gRange = div
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)');
 
-//   function createRangeSlider(data, column, rows) {
-//     // // Range
-
-//     var revs = data.map( function(d) {
-//         return parseFloat(d[column]);});
-    
-//     var sliderRange = d3
-//     .sliderBottom()
-//     .min(d3.min(revs, function(d) { return d; }))
-//     .max(d3.max(revs, function(d) { return d; }))
-//     .width(300)
-//     .tickFormat(function(d) {
-//         return myFormatter(column, d)
-//     })
-//     .ticks(5)
-//     .default([0,1])
-//     .fill('#2196f3')
-//     .on('onchange', val => {
-//         d3.select('p#value-range').text(val.map(d3.format('.2f')).join('-'));
-//         rows.filter(function(r) {
-//             return false;
-//         })
-//         console.log(val);
-//     });
-
-//     var gRange = d3
-//     .select('div#slider-range')
-//     .append('svg')
-//     .attr('width', 500)
-//     .attr('height', 100)
-//     .append('g')
-//     .attr('transform', 'translate(30,30)');
-
-//     gRange.call(sliderRange);
-
-//     d3.select('p#value-range').text(
-//     sliderRange
-//         .value()
-//         .map(d3.format('.2f'))
-//         .join('-')
-//     );
-// }
+    gRange.call(sliderRange);
+    // d3.select('p#value-range').text(
+    // sliderRange
+    //     .value()
+    //     .map(d3.format('.2f'))
+    //     .join('-')
+    // );
+}
