626436665e232d239cfcc33ea2c621b4b1e964eb
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2021-04-08 14:25:24 -0700

    Apr 21 info updates

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ particlesJS("particles-js", {
       }
     },
     "color": {
-      "value": "#000000"
+      "value": "#616161"
     },
     "shape": {
       "type": "circle",
@@ -50,7 +50,7 @@ particlesJS("particles-js", {
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#000000",
+      "color": "#616161",
       "opacity": 0.4,
       "width": 1
     },
